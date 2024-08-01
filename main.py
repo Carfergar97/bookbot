123bd7b25b1b8926ba2bc91cc86bf6cc9c06c797
Carfergar97 <cfdezg1997@gmail.com>
2024-08-01 17:37:06 +0200

    Added the count_character function

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ def count_words(book):
     words = book.split()
     return  len(words)
 
+def count_characters(book):
+    words = book.split()
+    character_count = { }
+    for word in words:
+        for char in word.lower():
+            if (char in character_count) == False:
+                character_count[char] = 1
+            else:
+                character_count[char] += 1
+    return character_count
+
 def get_book_text(book_filepath):
     with open(book_filepath) as f:
         file_contents = f.read()
@@ -12,6 +23,10 @@ def main():
 
     words_count = count_words(book_text)
     
+    characters_count = count_characters(book_text)
+
     print(f"The number of words in the book is {words_count}")
+    print(f"The number ocurrences of each character is {characters_count}")
+
 
 main()
